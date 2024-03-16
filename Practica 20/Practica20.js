b9d9fea7d4191c5e7e1dc3405c39d1340e5361d6
chriXico <hernandez.christopher@uabc.edu.mx>
2024-03-15 21:02:45 -0700

    Subimos practica 19

---
+++ Practica 20/Practica20.js
@@ -0,0 +1,14 @@
+var num1 = 5;
+var num2 = 8;
+
+if (num1 <= num2) {
+    console.log("num1 no es mayor que num2");
+}
+
+if (num2 > 0) {
+    console.log("num2 es positivo");
+}
+
+if (++num1 < num2) {
+    console.log("Incrementar uno en num1 no lo hace mayor que num2");
+}
