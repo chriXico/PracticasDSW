294461b11710d0b0598887ca49476b9185b63d51
chriXico <158236914+chriXico@users.noreply.github.com>
2024-04-05 17:27:56 -0700

    Subimos practica 24

---
+++ Practica 24/Practica24.js
@@ -0,0 +1,7 @@
+function avisoHuracan(hayUnHuracan) {
+    alert(hayUnHuracan ? "¡Hay un Huracán!" : "¡Está Soleado!");
+}
+
+// Ejemplo de uso:
+avisoHuracan(true); // Alerta: ¡Hay un Huracán!
+avisoHuracan(false); // Alerta: ¡Está Soleado!
